b5fb946c159707aa672b6a486d6a0b7e1fd18751
taskhanzada <39177236+taskhanzada@users.noreply.github.com>
2018-05-12 19:38:41 -0400

    Longest Word Problem SOLVED

--- longestword.js
+++ longestword.js
@@ -5,7 +5,15 @@
 
 
 function findLongestWord(str) {
-  return str.length;
+  let arr = str.split(" ");
+  let max = 0;
+  for(var i = 0; i < arr.length; i++){
+    if(max < arr[i].length){
+      max = arr[i].length
+    }
+  }
+
+  return max; 
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");

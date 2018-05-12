1029501ee6a532922ddfed462bce946493e2c00b
taskhanzada <39177236+taskhanzada@users.noreply.github.com>
2018-05-12 19:26:25 -0400

    Create longestword.js

---
+++ longestword.js
@@ -0,0 +1,11 @@
+//Find the Longest Word in a String 
+//Return the length of the longest word in the provided sentence.
+
+//Your response should be a number.
+
+
+function findLongestWord(str) {
+  return str.length;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");

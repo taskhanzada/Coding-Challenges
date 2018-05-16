1d536e6849ea8c47ab117537068421862ad77a69
taskhanzada <39177236+taskhanzada@users.noreply.github.com>
2018-05-16 00:44:21 -0400

    mutations_solved

--- mutations.js
+++ mutations.js
@@ -8,7 +8,17 @@
 
 
 function mutation(arr) {
-  return arr;
-}
+  let word1 = arr[0].toLowerCase();
+  let word2 = arr[1].toLowerCase();
 
-mutation(["hello", "hey"]);
+  
+  
+  for(var i = 0; i < word2.length;i++){
+    if(word1.indexOf(word2[i]) === -1){
+     return false;
+    }
+  }
+ return true;
+  
+}
+mutation(["hello", "hey"])

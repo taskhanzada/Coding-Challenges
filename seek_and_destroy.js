8a36ccf6351e594b26d8593d289e2194ba51bac4
taskhanzada <39177236+taskhanzada@users.noreply.github.com>
2019-12-30 02:06:45 -0500

    SOLVED BETTER

--- seek_and_destroy.js
+++ seek_and_destroy.js
@@ -1,9 +1,15 @@
 //You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
 
 
+
 function destroyer(arr) {
-  var args = [].slice.call(arguments).slice(1);
-  return arr.filter(val => (args.indexOf(val) === -1))
+
+  let foo = arr;
+  for(let i = 1; i < arguments.length; i++){
+   foo = (foo.filter(x => x !== arguments[i]));
+  }
+   return foo;
+  console.log(arguments)
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);

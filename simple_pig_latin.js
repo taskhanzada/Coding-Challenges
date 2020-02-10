2c24f4b110691482a05f292466d4de49cfdefce7
taskhanzada <39177236+taskhanzada@users.noreply.github.com>
2020-02-09 19:31:56 -0500

    SOLVED

--- simple_pig_latin.js
+++ simple_pig_latin.js
@@ -5,7 +5,15 @@
 // pigIt('Hello world !');     // elloHay orldway !
 
 function pigIt(str){
+  let strArr = str.split(' ');
+  let pigArr = strArr.map(function(element) {
+    if(element !== "!" && element !== "?"){
+  return (element.slice(1) + element.slice(0,1) + 'ay'); 
+    } else {
+      return element;
+    }
 
+  });
+  let finalpig = pigArr.join(' ');
+  return finalpig;
 }
-
-pigIt('Hello world !')

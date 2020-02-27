be904329cb3ca8568d747965851315dfc10522b0
taskhanzada <39177236+taskhanzada@users.noreply.github.com>
2020-02-27 08:53:08 -0500

    SOLVED

--- the-hashtag-generator.js
+++ the-hashtag-generator.js
@@ -12,7 +12,18 @@ The marketing team is spending way too much time typing in hashtags.
 // "    Hello     World   "                  =>  "#HelloWorld"
 // ""                                        =>  false
 
-
 function generateHashtag (str) {
+
+
+ let newArr = str.split(" ");
+ let updatedArr = newArr.map((x) => {
+   return x.charAt(0).toUpperCase() + x.slice(1);
+ })
+ let ans = updatedArr.join("")
+if(ans.length !== 0 && ans.length < 140){
+  return '#' + ans;
+} else{
+  return false
+}
 
 }
